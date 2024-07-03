34a7817ad203e750abf6fa3cd882e88b2dd53872
nurlanlan <nurahmad004@gmail.com>
2024-07-03 19:05:22 +0400

    overrided methods created at service classes

--- src/main/java/az/edu/turing/service/Impl/BookingService.java
+++ src/main/java/az/edu/turing/service/Impl/BookingService.java
@@ -1,4 +1,24 @@
 package az.edu.turing.service.Impl;
 
-public class BookingService {
+import az.edu.turing.model.BookingDto;
+import az.edu.turing.service.BookingInterface;
+
+import java.util.List;
+
+public class BookingService implements BookingInterface {
+
+    @Override
+    public List<BookingDto> findBookByPassengerName(String passengerName) {
+        return null;
+    }
+
+    @Override
+    public void cancelBooking(long id) {
+
+    }
+
+    @Override
+    public void bookFlight(BookingDto bookingDto) {
+
+    }
 }
--- src/main/java/az/edu/turing/service/Impl/FlightService.java
+++ src/main/java/az/edu/turing/service/Impl/FlightService.java
@@ -1,4 +1,41 @@
 package az.edu.turing.service.Impl;
 
-public class FlightService {
+import az.edu.turing.model.FlightDto;
+import az.edu.turing.service.FlightInterface;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+public class FlightService implements FlightInterface {
+    @Override
+    public List<FlightDto> findFlightsFromKievIn24Hours(String location, LocalDateTime dateTime) {
+        return null;
+    }
+
+    @Override
+    public Optional<FlightDto> findFlightById(long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<FlightDto> getFlightDetailsByFLightId(long id) {
+        return null;
+    }
+
+    @Override
+    public void createFlight(FlightDto flightDto) {
+
+    }
+
+    @Override
+    public List<FlightDto> getFlightsByDetails(String destination, LocalDateTime dateTime, int seats) {
+        return null;
+    }
+
+    @Override
+    public Collection<FlightDto> getAllFlights() {
+        return null;
+    }
 }
