348af097f95eb2010f08c6390cd259df635b4c9e
nurlanlan <nurahmad004@gmail.com>
2024-07-03 19:05:40 +0400

    controllers filled

--- src/main/java/az/edu/turing/contreller/BookingController.java
+++ src/main/java/az/edu/turing/contreller/BookingController.java
@@ -1,4 +1,26 @@
 package az.edu.turing.contreller;
 
+import az.edu.turing.model.BookingDto;
+import az.edu.turing.service.Impl.BookingService;
+
+import java.util.List;
+
 public class BookingController {
+    public BookingService bookingService;
+
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    public void cancelBooking(long id) {
+        bookingService.cancelBooking(id);
+    }
+
+    public void bookFlight(BookingDto bookingDto) {
+        bookingService.bookFlight(bookingDto);
+    }
+
+    public List<BookingDto> findBookByPassengerName(String passengerName) {
+        return bookingService.findBookByPassengerName(passengerName);
+    }
 }
--- src/main/java/az/edu/turing/contreller/FlightController.java
+++ src/main/java/az/edu/turing/contreller/FlightController.java
@@ -1,4 +1,39 @@
 package az.edu.turing.contreller;
 
+import az.edu.turing.dao.BookingDao;
+import az.edu.turing.model.FlightDto;
+import az.edu.turing.service.Impl.FlightService;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
 public class FlightController {
+
+    public FlightService flightService;
+
+    public List<FlightDto> findFlightsFromKievIn24Hours(String location, LocalDateTime dateTime) {
+        return flightService.findFlightsFromKievIn24Hours(location, dateTime);
+    }
+
+    public Optional<FlightDto> findFlightById(long id) {
+        return flightService.findFlightById(id);
+    }
+
+    public List<FlightDto> getFlightDetailsByFLightId(long id) {
+        return flightService.getFlightDetailsByFLightId(id);
+    }
+
+    public void createFlight(FlightDto flightDto) {
+        flightService.createFlight(flightDto);
+    }
+
+    public List<FlightDto> getFlightsByDetails(String destination, LocalDateTime dateTime, int seats) {
+        return flightService.getFlightsByDetails(destination, dateTime, seats);
+    }
+
+    public Collection<FlightDto> getAllFlights() {
+        return flightService.getAllFlights();
+    }
 }
