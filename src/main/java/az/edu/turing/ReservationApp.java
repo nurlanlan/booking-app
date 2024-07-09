359047f92746edd93a841e78fac48047ff76d3ca
nurlanlan <nurahmad004@gmail.com>
2024-07-09 22:29:25 +0400

    try running the app

--- src/main/java/az/edu/turing/ReservationApp.java
+++ src/main/java/az/edu/turing/ReservationApp.java
@@ -1,19 +1,12 @@
 package az.edu.turing;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import az.edu.turing.util.ConsoleUtil;
+
 public class ReservationApp {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ConsoleUtil console = new ConsoleUtil();
+        console.start();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
